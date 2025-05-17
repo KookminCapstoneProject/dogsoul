b2e7c9ee7a245ef7b35feb2c8667624c7c97b045
Juan Kim <anzuroid@gmail.com>
2025-05-17 23:43:51 +0900

    ok

--- Assets/Scripts/Cinematic/Opening.cs
+++ Assets/Scripts/Cinematic/Opening.cs
@@ -15,38 +15,14 @@ public class Opening : MonoBehaviour
         public string[] scripts;
     }
 
+    public Image backgorund;
     public Image splashImage;
     public TextMeshProUGUI[] texts;
     public SplashPage[] pages;
 
-    private int currentIndex = 0;
-
     void Start()
     {
-        ShowPage(currentIndex);
-    }
-
-    void Update()
-    {
-        if (Input.anyKeyDown)
-        {
-            currentIndex++;
-            if (currentIndex < pages.Length)
-            {
-                ShowPage(currentIndex);
-            }
-            else
-            {
-                LoadNextScene(); // 씬전환 or 오브젝트 삭제
-            }
-        }
-    }
-
-    void ShowPage(int index)
-    {
-        splashImage.sprite = pages[index].image;
-        print(index);
-        StartCoroutine(TypingText(index));
+        StartCoroutine(TypingText());
     }
 
     void LoadNextScene()
@@ -54,31 +30,45 @@ public class Opening : MonoBehaviour
         // UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
     }
 
-    public IEnumerator TypingText(int index) {
-        while (true) {
-            //=======================================================================================================
-            // Initializing | 초기화
-            //=======================================================================================================
-            foreach (TextMeshProUGUI t in texts)
-                t.text = "";
-            //=======================================================================================================
+    public IEnumerator TypingText()
+    {
+        for (int index = 0; index < pages.Length; index++) // 페이지 불러오기
+        {
+            splashImage.sprite = pages[index].image; // 이미지 전환
+            foreach (TextMeshProUGUI t in texts) t.text = "";
 
-
-            //=======================================================================================================
-            //  Typing effect | 타이핑 효과
-            //=======================================================================================================
-            for (int t = 0; t < texts.Length && t < pages[index].scripts.Length; t++) {
+            for (int t = 0; t < texts.Length && t < pages[index].scripts.Length; t++) // 문장 불러오기
+            {
                 int strTypingLength = pages[index].scripts[t].GetTypingLength();
 
-                for (int i = 0; i <= strTypingLength; i++) {
+                for (int i = 0; i <= strTypingLength; i++) // 한국어 타이핑
+                {
                     texts[t].text = pages[index].scripts[t].Typing(i);
-                    yield return new WaitForSeconds(0.03f);
+                    yield return new WaitForSeconds(0.02f);
                 }
-                // Wait 1 second per 1 sentence | 한 문장마다 1초씩 대기
-                yield return new WaitForSeconds(1f);
+                yield return new WaitForSeconds(0.5f);
             }
-            // Wait 1 second at the end | 마지막에 1초 추가 대기함
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(0.5f);
         }
+        StartCoroutine(CoFadeOut());
+    }
+
+    IEnumerator CoFadeOut()
+    {
+        float elapsedTime = 0f; 
+        float fadedTime = 3f; 
+
+        while (elapsedTime <= fadedTime)
+        {
+            backgorund.GetComponent<CanvasRenderer>().SetAlpha(Mathf.Lerp(1f, 0f, elapsedTime / fadedTime));
+            splashImage.GetComponent<CanvasRenderer>().SetAlpha(Mathf.Lerp(1f, 0f, elapsedTime / fadedTime));
+            foreach (TextMeshProUGUI t in texts) t.GetComponent<CanvasRenderer>().SetAlpha(Mathf.Lerp(1f, 0f, elapsedTime / fadedTime));
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
+        yield break;
     }
 }
