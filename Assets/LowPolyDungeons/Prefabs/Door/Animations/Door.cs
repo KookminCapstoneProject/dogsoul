325a6519cd179c61bbbaa8775c316f11b6e98013
dup06570 <dup06570@kookmin.ac.kr>
2025-05-20 21:07:02 +0900

    Merge test

--- Assets/LowPolyDungeons/Prefabs/Door/Animations/Door.cs
+++ Assets/LowPolyDungeons/Prefabs/Door/Animations/Door.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Door : InteractGo
 {
-    [SerializeField] AudioClip openSound;
-    [SerializeField] [Range(0, 1)] float openVolume = 1f;
     [SerializeField] private Animator animator;
     private bool isOpen = false;
 
@@ -23,7 +21,6 @@ public class Door : InteractGo
     public override void InteractObject()
     {
         isOpen = !isOpen;
-        AudioSource.PlayClipAtPoint(openSound, transform.position, openVolume);
         animator.SetBool("isOpen", isOpen);
     }
 
